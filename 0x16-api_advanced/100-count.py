28250887ae2aba26f82b2c7f4d66fb3f18028041
JNicolao <bororio96@gmail.com>
2023-06-22 12:03:15 +0300

    Updated count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,78 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddiit subrreddit."""
-import requests
+""" This script contains function that queries the Reddit API,
+    parses the title of all hot articles, and prints a sorted count
+    of given keywords.
+
+    Globals:
+        @posts: the list of a hot posts from a given subreddit
+        @after: the link that keeps track of a pagination
+"""
+from requests import get
+from sys import argv
+
+posts = []
+after = None
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def print_counts(posts, word_list):
+    """ Prints a sorted count of given keywords (case-insensitive)
+
+        Args:
+            @posts: list of a hottest posts
+            @word_list: a list of keywords to print
+
+        Globals:
+            @posts: the list of a hot posts from a given subreddit
+            @after: the link that keeps track of a pagination
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686 on x86_64)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+    results = {}
+    for word in word_list:
+        results[word.lower()] = 0
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+    for title in posts:
+        words = title.split(" ")
+        for word in words:
+            if results.get(word) is not None:
+                results[word] += 1
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+    keywords = sorted(results, key=results.get, reverse=True)
+    for kword in keywords:
+        if results.get(kword):
+            for word in word_list:
+                if kword == word.lower():
+                    print("{}: {}".format(word, results[kword]))
+
+
+def count_words(subreddit, word_list):
+    """ Queries the Reddit API, parses the title of all hot articles,
+        prints a sorted count of given keywords (case-insensitive,
+        delimited by spaces. E.g., JavaScript should count as javascript,
+        but java should not).
+
+        Args:
+            @subreddit: a subreddit to retrieve
+            @word_list: a list of keywords to search
+    """
+    global posts
+    global after
+
+    headers = {"User-Agent": "0x16. API advanced"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    if after:
+        url = url + "?after={}".format(after)
+
+    count = get(url, headers=headers).json().get("data")
+
+    for post in count.get("children"):
+        posts.append(post.get("data").get("title").lower())
+
+    after = count.get("after")
+    if after is not None:
+        return count_words(subreddit, word_list)
+
+    return print_counts(posts, word_list)
+
+
+if __name__ == "__main__":
+    count_words(argv[1], argv[2].split(" "))
